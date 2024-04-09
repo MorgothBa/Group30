88eb1e0ead191ce66b93b65f6bab858d01bb6d24
Torrente1382 <66202619+Torrente1382@users.noreply.github.com>
2024-04-09 12:33:32 +0200

    Update HomeController.cs

--- Moodle/Controllers/HomeController.cs
+++ Moodle/Controllers/HomeController.cs
@@ -1,10 +1,14 @@
 using Microsoft.AspNetCore.Mvc;
 using Moodle.Models;
 using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace Moodle.Controllers
 {
-    public class HomeController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
     {
         private readonly ILogger<HomeController> _logger;
 
@@ -13,20 +17,30 @@ namespace Moodle.Controllers
             _logger = logger;
         }
 
+        // GET: api/Home/Index
+        [HttpGet("Index")] 
+        [SwaggerOperation(Summary = "Get index")]
         public IActionResult Index()
         {
-            return View();
+            return Ok();
         }
 
+        // GET: api/Home/Privacy
+        [HttpGet("Privacy")] 
+        [SwaggerOperation(Summary = "Get privacy")]
         public IActionResult Privacy()
         {
-            return View();
+            return Ok();
         }
 
+        // GET: api/Home/Error
+        [HttpGet("Error")] 
+        [SwaggerOperation(Summary = "Get error")]
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return Ok(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
+
